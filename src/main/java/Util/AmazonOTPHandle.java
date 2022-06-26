dc09c608bb567398bc2d1f2e2a713709fb9c6fdf
Dipika595 <karmakar.dipika595@gmail.com>
2022-06-27 01:34:19 +0530

    new commit

--- src/main/java/Util/AmazonOTPHandle.java
+++ src/main/java/Util/AmazonOTPHandle.java
@@ -24,7 +24,7 @@ public class AmazonOTPHandle {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "/Users/NaveenKhunteta/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
